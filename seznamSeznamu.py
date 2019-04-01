3853683484d90665f46248e841276d52629989af
Ivana Kráčková <kralickovaivana@gmail.com>
2019-04-01 18:24:13 +0200

    Pridaní seznamSeznamu.py

---
+++ seznamSeznamu.py
@@ -0,0 +1,19 @@
+seznam = [[5],[5],[5],[5], [5],[5]]
+
+for i in range(13):
+    seznam == seznam.append(seznam[seznam[5][0]])
+seznam ==seznam.append(seznam[5][0])
+print(seznam)
+if seznam != 5:
+    print("blbě")
+
+
+# seznam = [[5],[5],[5],[5], [5],[5]]
+# seznam=[[5],[5],[5],[5], [5],[5]]
+# for i in range(20):
+#     seznam==seznam.append(seznam[seznam[5][0]])
+#     seznam_seznamu==seznam
+# seznam.append(seznam_seznamu[5][0])
+# print(seznam)
+# if seznam_seznamu !=5:
+#     print("blbě")
